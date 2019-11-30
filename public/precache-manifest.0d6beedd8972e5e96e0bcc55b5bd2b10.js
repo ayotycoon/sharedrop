37ee1c2310279bdbacb97ba11c6416f614394057
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2019-11-30 13:24:25 +0100

    first

--- public/precache-manifest.0d6beedd8972e5e96e0bcc55b5bd2b10.js
+++ public/precache-manifest.0d6beedd8972e5e96e0bcc55b5bd2b10.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "416efd563338b1e231a1071375eccc47",
+    "revision": "6e25e329b4183cc31c64d1d8a574519a",
     "url": "/index.html"
   },
   {
-    "revision": "12074c9ef5c0ed48b31a",
+    "revision": "cdd8041bc3e24fd0a444",
     "url": "/static/css/main.3fc62b28.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.40700560.chunk.js"
   },
   {
-    "revision": "12074c9ef5c0ed48b31a",
-    "url": "/static/js/main.2a69c76e.chunk.js"
+    "revision": "cdd8041bc3e24fd0a444",
+    "url": "/static/js/main.eeed4d37.chunk.js"
   },
   {
     "revision": "60c0635d59f291582c8a",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1be484991f813b80821b2f9cbdaeb503.js"
+  "/precache-manifest.0d6beedd8972e5e96e0bcc55b5bd2b10.js"
 );
 
 self.addEventListener('message', (event) => {
